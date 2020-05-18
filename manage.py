f70e95f46b6914f3edc77507453490c1a52d4f64
liuqiang <1984145803@qq.com>
2020-05-18 11:30:46 +0800

    项目配置V5.0-蓝图目录创建

--- manage.py
+++ manage.py
@@ -5,16 +5,11 @@ from info import db, create_app
 
 # 创建 app，并传入配置模式：development / production
 app = create_app('development')
-
+# 添加扩展命令行
 manager = Manager(app)
+# 数据库迁移
 Migrate(app, db)
 manager.add_command('db', MigrateCommand)
-
-
-@app.route('/index')
-def index():
-    return 'index'
-
 
 if __name__ == '__main__':
     app.run(port=5000, host='0.0.0.0')
