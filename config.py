808a39ea3bd1d2769023d6787211865e5aea48df
liuqiang <1984145803@qq.com>
2020-05-18 10:36:42 +0800

    项目配置V3.0-代码抽取

---
+++ config.py
@@ -0,0 +1,31 @@
+import redis
+
+
+class Config(object):
+    DEBUG = True
+    SECRET_KEY = "EjpNVSNQTyGi1VvWECj9TvC/+kq3oujee2kTfQUs8yCM6xX9Yjq52v54g+HVoknA"
+    SQLALCHEMY_DATABASE_URI = "mysql://root:mysql@47.102.102.179:3306/information"
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    REDIS_HOST = "47.102.102.179"
+    REDIS_PORT = 6379
+    # flask_session的配置信息
+    SESSION_TYPE = "redis"  # 指定 session 保存到 redis 中
+    SESSION_USE_SIGNER = True  # 让 cookie 中的 session_id 被加密签名处理
+    SESSION_REDIS = redis.StrictRedis(host=REDIS_HOST, port=REDIS_PORT)  # 使用 redis 的实例
+    PERMANENT_SESSION_LIFETIME = 86400  # session 的有效期，单位是秒
+
+
+class DevelopementConfig(Config):
+    """开发模式下的配置"""
+    DEBUG = True
+
+
+class ProductionConfig(Config):
+    """生产模式下的配置"""
+    pass
+
+# 定义配置字典
+config = {
+    "development": DevelopementConfig,
+    "production": ProductionConfig
+}
